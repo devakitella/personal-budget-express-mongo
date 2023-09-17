4089bd6f2b01c0fc134a1cfa7576dfae0ef0b92e
devakitella <devakitella@gmail.com>
2023-09-17 03:34:23 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.use("/", express.static("public"));
-
-const budget = {
-  myBudget: [
-    {
-      title: "Eat out",
-      budget: 30,
-    },
-    {
-      title: "Rent",
-      budget: 350,
-    },
-    {
-      title: "Groceries",
-      budget: 90,
-    }
-  ]
-};
 
 app.get("/hello", (req, res) => {
   res.send("Hello World from Devaki Tella!");
 });
 
 app.get("/budget", (req, res) => {
-  res.json(budget);
+  fs.readFile("budgetData.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading data");
+      return;
+    }
+    const budgetData = JSON.parse(data);
+    res.json(budgetData);
+  });
 });
 
 app.listen(port, () => {
