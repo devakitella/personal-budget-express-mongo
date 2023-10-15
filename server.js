8b8e10aaead4d7b8794f4c6b41013991a713e779
devakitella <devakitella@gmail.com>
2023-10-15 10:06:28 -0400

    Updated-with-calls-for-add/fetch-data-from mongo

--- server.js
+++ server.js
@@ -5,31 +5,69 @@ const port = 3000;
 
 app.use("/", express.static("public"));
 
+const mongoose = require('mongoose');
+const Budget =  require('./models/budgetschema')
+let url = 'mongodb://127.0.0.1:27017/testing-db';
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(
+    () => (
+        console.log("Connected")
+    )
+).catch(
+    (connectionError) => console.log("Error while connecting to db", connectionError)
+    );
+
 app.get("/hello", (req, res) => {
   res.send("Hello World from Devaki Tella!");
 });
 
 app.get("/budget", (req, res) => {
-  fs.readFile("budgetData.json", "utf8", (err, data) => {
+  Budget.find({}, (err, data) => {
     if (err) {
-      console.error(err);
-      res.status(500).send("Error reading data");
-      return;
+        console.error('Error querying the database:', err);
+        res.status(500).json({ error: 'Error querying the database' });
+    } else {
+        res.json(data);
     }
-    const budgetData = JSON.parse(data);
-    res.json(budgetData);
   });
 });
 
 app.get("/example", (req, res) => {
-  fs.readFile("example.json", "utf8", (err, data) => {
+  Budget.find({}, (err, data) => {
     if (err) {
-      console.error(err);
-      res.status(500).send("Error reading data");
-      return;
+        console.error('Error querying the database:', err);
+        res.status(500).json({ error: 'Error querying the database' });
+    } else {
+        res.json(data);
     }
-    const exampleData = JSON.parse(data);
-    res.json(exampleData);
+  });
+});
+
+app.use(express.json());       
+app.use(express.urlencoded());
+
+app.post('/addBudgetItem', (req, res) => {
+
+  console.log(req.body)
+  const { title, budget, color } = req.body;
+  const newBudgetItem = new Budget({
+      title,
+      budget,
+      color,
+  });
+
+  // Save the new item to the database
+  newBudgetItem.save((err, savedItem) => {
+      if (err) {
+          console.error('Error saving budget item:', err);
+          res.status(500).json({ error: 'Error saving budget item' });
+      } else {
+          console.log('Budget item saved:', savedItem);
+          res.status(201).json({"success": "true","data": savedItem}); // Respond with the saved item
+      }
   });
 });
 
